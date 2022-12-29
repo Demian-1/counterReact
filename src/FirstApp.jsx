fbb1ed4647babc7cff088a0c7a21e60b04dbf7f6
Demian-1 <sergio_demian_ae@hotmail.com>
2022-12-28 23:52:04 -0600

    FRAGMENTOOSSS NENA, FRAGMENTOS

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,7 +1,10 @@
-
+// para no tener que usar un div y poder romper los estilos se usa <Fragment>
 
 export const FirstApp = () => {
   return (
-    <h2>FirstApp</h2>
+    <>
+        <h2>FirstApp</h2>
+        <p>Soy un subtitulo</p>
+    </>
   )
 }
