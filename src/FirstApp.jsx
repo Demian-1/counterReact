7396e51c84b6c867e18f9d4e981f0006e5856d90
Demian-1 <sergio_demian_ae@hotmail.com>
2022-12-29 23:36:59 -0600

    Funcioes y objetos en html (no se puede renderizar directamente objetos)

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,9 +1,17 @@
 // para no tener que usar un div y poder romper los estilos se usa <Fragment>
 
-export const FirstApp = () => {
+const nombree = {
+    message : 'holaa',
+    title: 'demians'
+};  // Si no cambia, o no tiene nada que ver con hooks se puede poner fuera del functional comp
+
+const saluda = (nombre) =>`hola amigo ${nombre}, a ver si ya terminas de aprender React`;
+
+export const FirstApp = () => {// no se puede poner un objeto, pero si se puede serializar
   return (
     <>
-        <h2>FirstApp</h2>
+        {/*<h2>{ JSON.stringify(nombree)  }</h2>*/}
+        <h2>{ saluda('Demian') }</h2>
         <p>Soy un subtitulo</p>
     </>
   )
