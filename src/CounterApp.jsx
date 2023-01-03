4a947f3e41861622e3801f5e283df12427cdd2c4
Demian-1 <sergio_demian_ae@hotmail.com>
2023-01-02 21:23:19 -0600

    Eventos

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,10 +1,22 @@
 import PropTypes from 'prop-types';
 
+// const handleAdd = ( event ) => {console.log(event)}; Si no ocupa nada de las props, entonces se puede poner afuera
+
 export const CounterApp = ({value}) => {
+
+    const handleAdd = ( value ) => {
+        console.log('+1');
+        value = 100;
+    };  
+
   return (
     <>
         <h1>CounterApp</h1>
-        <h2>{value}</h2>
+        <h2> { value } </h2>
+
+        <button role="button" className="button-74" onClick={ handleAdd }>
+            +1
+        </button>
     </>
   );
 }
