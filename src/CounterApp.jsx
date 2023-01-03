f99276bcca6c3052a83fcda11cd0024f094984f8
Demian-1 <sergio_demian_ae@hotmail.com>
2023-01-02 21:51:22 -0600

    tarea final del counter

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,21 +1,39 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 
 // const handleAdd = ( event ) => {console.log(event)}; Si no ocupa nada de las props, entonces se puede poner afuera
 
 export const CounterApp = ({value}) => {
 
-    const handleAdd = ( value ) => {
-        console.log('+1');
-        value = 100;
-    };  
+    const [ counter, setCounter ] = useState(value);
+
+    const handleAdd = () => {
+        // console.log(event)
+        setCounter(counter+1);
+        // setCounter( (c) => c+1 )  el valor c es counter
+    };
+
+    const handleSubstract = () => {
+        setCounter(counter-1);
+    }
+
+    const handleReset = () => {
+        setCounter(value);
+    }
 
   return (
     <>
         <h1>CounterApp</h1>
-        <h2> { value } </h2>
+        <h2> { counter } </h2>
 
         <button role="button" className="button-74" onClick={ handleAdd }>
             +1
+        </button>
+        <button role="button" className="button-74" onClick={ handleSubstract }>
+            -1
+        </button>
+        <button role="button" className="button-74" onClick={ handleReset }>
+            Reset
         </button>
     </>
   );
