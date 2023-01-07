35c53d7f5e727f5f200ae9a333559b160d0a40ce
Demian-1 <sergio_demian_ae@hotmail.com>
2023-01-06 23:29:11 -0600

    asi se hacen mejor las pruebas

--- tests/demo.test.js
+++ tests/demo.test.js
@@ -1,1 +1,11 @@
-test('esta prueba no debe de fallar', () => {  })
+test('esta prueba no debe de fallar', () => { 
+    
+    // 1. inicializacion
+    const message1 = 'Hola mundo';
+
+    // 2. estimulo
+    const message2 = message1.trim();
+
+    // 3. observar el compostamient esperado
+    expect(message1).toBe(message1);
+ })
